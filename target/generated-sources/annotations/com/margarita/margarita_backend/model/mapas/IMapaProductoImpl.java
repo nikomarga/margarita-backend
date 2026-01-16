a6a1e59381d3583fe19f99700e3c43a8f4c5374c
Nikole carrasquel <nikole.margarita.carrasquel@gmail.com>
2026-01-16 14:11:54 -0500

    Dockerfilenueva

---
+++ target/generated-sources/annotations/com/margarita/margarita_backend/model/mapas/IMapaProductoImpl.java
@@ -0,0 +1,48 @@
+package com.margarita.margarita_backend.model.mapas;
+
+import com.margarita.margarita_backend.model.Product;
+import com.margarita.margarita_backend.model.dtos.ProductoGenericoDTO;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2026-01-16T14:05:30-0500",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.2 (Oracle Corporation)"
+)
+@Component
+public class IMapaProductoImpl implements IMapaProducto {
+
+    @Override
+    public ProductoGenericoDTO convertirDTO(Product producto) {
+        if ( producto == null ) {
+            return null;
+        }
+
+        ProductoGenericoDTO productoGenericoDTO = new ProductoGenericoDTO();
+
+        productoGenericoDTO.setNombre( producto.getNombre() );
+        productoGenericoDTO.setDescripcion( producto.getDescripcion() );
+        productoGenericoDTO.setPrecio( producto.getPrecio() );
+        productoGenericoDTO.setCategoria( producto.getCategoria() );
+        productoGenericoDTO.setImagen( producto.getImagen() );
+
+        return productoGenericoDTO;
+    }
+
+    @Override
+    public List<ProductoGenericoDTO> convertirListaADTO(List<Product> products) {
+        if ( products == null ) {
+            return null;
+        }
+
+        List<ProductoGenericoDTO> list = new ArrayList<ProductoGenericoDTO>( products.size() );
+        for ( Product product : products ) {
+            list.add( convertirDTO( product ) );
+        }
+
+        return list;
+    }
+}
