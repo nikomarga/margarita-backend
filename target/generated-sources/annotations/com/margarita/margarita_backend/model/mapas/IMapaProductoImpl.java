b7e3ae34617bd60a8a552830d2d38c945d107aad
Nikole carrasquel <nikole.margarita.carrasquel@gmail.com>
2026-01-25 16:48:38 -0500

    fix: force H2 datasource and dialect for render

--- target/generated-sources/annotations/com/margarita/margarita_backend/model/mapas/IMapaProductoImpl.java
+++ target/generated-sources/annotations/com/margarita/margarita_backend/model/mapas/IMapaProductoImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-01-20T14:08:07-0500",
+    date = "2026-01-25T16:48:19-0500",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.2 (Oracle Corporation)"
 )
 @Component
