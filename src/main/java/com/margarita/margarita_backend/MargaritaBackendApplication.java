9657dc6a83e16e5cd18c0058c10a64de681de7f2
Nikole carrasquel <nikole.margarita.carrasquel@gmail.com>
2026-01-25 18:00:58 -0500

    fix: Aplication

--- src/main/java/com/margarita/margarita_backend/MargaritaBackendApplication.java
+++ src/main/java/com/margarita/margarita_backend/MargaritaBackendApplication.java
@@ -2,11 +2,25 @@ package com.margarita.margarita_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MargaritaBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MargaritaBackendApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://127.0.0.1:5500") // tu frontend local
+						.allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
 	}
 }
